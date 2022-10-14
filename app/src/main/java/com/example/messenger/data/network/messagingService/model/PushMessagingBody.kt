12e9ce666fbbd4c77c48a43bd71c54d63f6d35c7
ISPr24-39_BeluakovDS <dan.bel.89@bk.ru>
2022-10-14 17:11:18 +0400

    version 1.1

---
+++ app/src/main/java/com/example/messenger/data/network/messagingService/model/PushMessagingBody.kt
@@ -0,0 +1,4 @@
+package com.example.messenger.data.network.messagingService.model
+
+class PushMessagingBody {
+}
---
+++ app/src/main/java/com/example/messenger/data/network/messagingService/repository/MessagingServiceRepository.kt
@@ -0,0 +1,4 @@
+package com.example.messenger.data.network.messagingService.repository
+
+class MessagingServiceRepository {
+}
